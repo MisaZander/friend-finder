f8f370ad9a412739a8dca538e63c9318f8a0acd1
Alexander Osborn <misterzander@gmail.com>
2018-11-11 16:36:07 -0600

    Restauant tasks

---
+++ app/data/survey.js
@@ -0,0 +1,16 @@
+var questions = {
+    question: [
+        "I prefer a night home alone than a night out on most occasions.",
+        "I believe in a certain level of give and take in a true friendship.",
+        "I prefer to confide in anonymous strangers online that with real people.",
+        "I tend to replay conversations over and over wondering where I went wrong.",
+        "I will in most cases reply to texts or online communiques as soon as humanly possible.",
+        "I do a lot of online digging on a person before or shortly after I first meet them.",
+        "I secretly think the people around me hate me even though that's most likely logically untrue.",
+        "I probably spend at least 300% more time in the bathroom than a normal person if I take my phone in there.",
+        "I'm not entirely sure what I would do with my days if the Internet went down.",
+        "I like to stay up to date with current events rather than live under a rock."
+    ]    
+}
+
+module.exports = questions;
